4071f280276ec69b8679ca9f867e2c30a2d88b4f
Josiah Aviso <150400184+JoshAviso@users.noreply.github.com>
2026-01-15 21:05:17 +0800

    grids!

---
+++ Assets/_Utils/Grid/Grid.cs
@@ -0,0 +1,59 @@
+
+using System;
+using UnityEngine;
+
+public interface IGridObject
+{
+    public uint XSize { get; }
+    public uint YSize { get; }
+    public uint ZSize { get; }
+}
+[Serializable] public class Grid<TGridObject>
+{
+    public uint XSize { get; private set; }
+    public uint YSize { get; private set; }
+    public uint ZSize { get; private set; }
+    public TGridObject[,,] _objects;
+    public void Resize(uint newX, uint newY, uint newZ)
+    {
+        Debug.Log("Running Resize");
+        if (_objects == null)
+        {
+            _objects = new TGridObject[newX, newY, newZ];
+            return;
+        }
+
+        TGridObject[,,] newArray = new TGridObject[newX, newY, newZ];
+
+        uint minX = Math.Min(XSize, newX);
+        uint minY = Math.Min(YSize, newY);
+        uint minZ = Math.Min(ZSize, newZ);
+
+        XSize = newX; 
+        YSize = newY; 
+        ZSize = newZ;
+        
+        for (uint x = 0; x < minX; ++x)
+        for (uint y = 0; y < minY; ++y)
+        for (uint z = 0; z < minZ; ++z)
+            newArray[x, y, z] = _objects[x, y, z];
+
+        _objects = newArray;
+    }
+
+    public TGridObject GetObjectAtCell(uint x, uint y, uint z)
+        { return IsWithinBounds(x, y, z) ? _objects[x, y, z] : default(TGridObject); }
+
+    /// <returns>True if the grid address is within the grid's dimensions</returns>
+    public bool SetObjectAt(uint x, uint y, uint z, TGridObject gridObject)
+    {
+        if(!IsWithinBounds(x, y, z)) return false;
+        _objects[x, y, z] = gridObject;
+        return true;
+    }
+
+    public bool IsWithinBounds(uint x, uint y, uint z)
+        { return !(x >= XSize || y >= YSize || z >= ZSize); }
+    public bool IsWithinBounds(int x, int y, int z)
+        { return !(x < 0 || y < 0 || z < 0 || x >= XSize || y >= YSize || z >= ZSize); }
+}
